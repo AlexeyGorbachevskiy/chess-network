dc5aae540f53e7631847a8315701e62deb12cc30
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-12-06 21:30:10 +0300

    Updates

---
+++ precache-manifest.e412dea092e51d49952d04240d6d3663.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598d52990391b701269cd75e1f823890",
+    "url": "/chess-network/index.html"
+  },
+  {
+    "revision": "a2ad0241c658bd7e9773",
+    "url": "/chess-network/static/css/2.f57da490.chunk.css"
+  },
+  {
+    "revision": "38a57c6033aa1036346c",
+    "url": "/chess-network/static/css/main.fbcf7645.chunk.css"
+  },
+  {
+    "revision": "a2ad0241c658bd7e9773",
+    "url": "/chess-network/static/js/2.1fcc80cd.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/chess-network/static/js/2.1fcc80cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a57c6033aa1036346c",
+    "url": "/chess-network/static/js/main.6e1d9e9f.chunk.js"
+  },
+  {
+    "revision": "a63478449513ab6ce04c",
+    "url": "/chess-network/static/js/runtime-main.1e9655ad.js"
+  },
+  {
+    "revision": "244242ad3c585594a8d23c4d8bd468fa",
+    "url": "/chess-network/static/media/1.244242ad.jpg"
+  },
+  {
+    "revision": "f8295d94f819fe81ab041b1992fc2651",
+    "url": "/chess-network/static/media/1.f8295d94.jpg"
+  },
+  {
+    "revision": "201610b490db7d527540a59408a5a5e7",
+    "url": "/chess-network/static/media/2.201610b4.jpg"
+  },
+  {
+    "revision": "52d7a986227d1e18cb0f38712dc2fc5c",
+    "url": "/chess-network/static/media/3.52d7a986.jpg"
+  },
+  {
+    "revision": "a62d68b5bd6454453aff3422e74212bc",
+    "url": "/chess-network/static/media/dud.a62d68b5.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/chess-network/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/chess-network/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/chess-network/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/chess-network/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/chess-network/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cff412dce612798ba29f6f3d59facde9",
+    "url": "/chess-network/static/media/harik.cff412dc.jpg"
+  },
+  {
+    "revision": "ea6d47142300ed0e29d64734d1247881",
+    "url": "/chess-network/static/media/kirill.ea6d4714.jpg"
+  },
+  {
+    "revision": "16ccc3c21c5d0d64be20f68ed2642e60",
+    "url": "/chess-network/static/media/saimon.16ccc3c2.jpg"
+  },
+  {
+    "revision": "b5af49e21a2f43afce29e2ee04a7010f",
+    "url": "/chess-network/static/media/vodonaeva.b5af49e2.jpg"
+  }
+]);
